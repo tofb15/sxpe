cbea3db920b27080f236bf229706f1408b3153f7
tofb15 <fas_th@hotmail.com>
2026-09-01 23:30:43 +0200

    Bump version from 0.3.0 to 0.4.0.
    Signed-off-by: SXPE contributors <sxpe@noreply.invalid>

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char** argv) {
     QApplication app(argc, argv);
     app.setApplicationName("SXPE");
     app.setOrganizationName("SXPE");
-    app.setApplicationVersion("0.3.0");
+    app.setApplicationVersion("0.4.0");
     apply_theme(app);
 
     sxpe::gui::MainWindow w;
--- src/mcp/main.cpp
+++ src/mcp/main.cpp
@@ -83,7 +83,7 @@ void write_message(const json& msg) {
 json initialize_result() {
     return {{"protocolVersion", "2026-07-28"},
             {"capabilities", {{"tools", {{"listChanged", false}}}}},
-            {"serverInfo", {{"name", "sxpe"}, {"version", "0.3.0"}}}};
+            {"serverInfo", {{"name", "sxpe"}, {"version", "0.4.0"}}}};
 }
 
 json tools_list(Bus& bus, const json& params) {
